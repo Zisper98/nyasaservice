8efb4d90258d32a0bf2d64968723440cda1ec765
Zisper98 <65983682+Zisper98@users.noreply.github.com>
2024-05-21 19:50:34 +0200

    Delete index.php

--- index.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-?><?php define("IN_SCRIPT","1"); error_reporting(0); session_start(); require("include/sm.php"); $website = new SiteManager(); $website->lggz("data/listings.xml"); $website->hdsy(); $website->lzmwjqh(); if(isset($_REQUEST["page"])) { $website->fgxfaq($_REQUEST["page"]); $website->voilrvd($_REQUEST["page"]); } $website->psfayo(); ?>
